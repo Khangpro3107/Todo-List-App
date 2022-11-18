1d7a3296edca54bbbf3aec09da0368f1d99b0717
unknown <khangpro31072002@gmail.com>
2022-11-19 06:35:45 +0700

    change server port

--- server/server.js
+++ server/server.js
@@ -116,4 +116,4 @@ app.post("/register/find", async (req, res) => {
   }
 })
 
-app.listen(process.env.PORT, console.log(`Server listening on port ${process.env.PORT}!`));
+app.listen(process.env.PORT || 80, console.log(`Server listening on port ${process.env.PORT}!`));
